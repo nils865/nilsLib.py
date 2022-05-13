6d5e15652cfd01d71aeacfeee076e9b37619b2e3
nils865 <nils.staas0806@gmail.com>
2022-05-13 12:14:20 +0200

    Cursor move function

--- nilsLib.py
+++ nilsLib.py
@@ -1,6 +1,4 @@
-
-import os
-
+import os, sys
     
 def clampDecimalPlace(comma, number): #Clamps the number to a specified number of decimal Places (e.g 0.123 >0.1)
   return int(number * (10 ** comma)) / (10 ** comma)
@@ -15,3 +13,5 @@ def setTTYBgCol(r,g,b): #Sets the color of the text background (if supported by 
 def clear(): #Clears the console
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def moveCursor(lines): # moves the cursor up by given lines
+   sys.stdout.write("\x1b[1A" * lines)
