6803bdc68e2e691d1293d59f1c6838059cffe93a
nils865 <nils.staas0806@gmail.com>
2022-05-13 15:19:51 +0200

    Added resetColor

--- nilslib/nlib.py
+++ nilslib/nlib.py
@@ -1,16 +1,19 @@
 import os, sys
     
-def clampDecimalPlace(comma, number): #Clamps the number to a specified number of decimal Places (e.g 0.123 >0.1)
+def clampDecimalPlace(comma, number): # Clamps the number to a specified number of decimal Places (e.g 0.123 >0.1)
   return int(number * (10 ** comma)) / (10 ** comma)
 
-def setTTYFgCol(r,g,b): #Sets the color of the text foreground (if supported by the terminal)
+def setTTYFgCol(r,g,b): # Sets the color of the text foreground (if supported by the terminal)
     return(f"\x1b[38;2;{r};{g};{b}m")
 
-def setTTYBgCol(r,g,b): #Sets the color of the text background (if supported by the terminal)
+def setTTYBgCol(r,g,b): # Sets the color of the text background (if supported by the terminal)
     return(f"\x1b[48;2;{r};{g};{b}m")
 
-def clear(): #Clears the console
+def clear(): # Clears the console
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def moveCursor(lines): # moves the cursor up by given lines
+def moveCursor(lines): # Moves the cursor up by given lines
    sys.stdout.write("\x1b[1A" * lines)
+
+def resetColor(): # Resets the color to default
+    sys.stdout.write("\x1b[0m")
