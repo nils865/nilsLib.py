5f3563c3ab0f3437810c954e92f3490202aaa23a
kruemmelbande <69861550+kruemmelbande@users.noreply.github.com>
2022-05-13 12:07:46 +0200

    Clear naming of the functions and added comments

--- nilsLib.py
+++ nilsLib.py
@@ -1,8 +1,8 @@
-def clampTo(comma, number):
+def clampDecimalPlace(comma, number): #Clamps the number to a specified number of decimal Places (e.g 0.123 >0.1)
     return int(number * (10 ** comma)) / (10 ** comma)
 
-def setColor(r,g,b):
+def setTTYfgCol(r,g,b): #Sets the color of the text foreground (if supported by the terminal)
     return(f"\x1b[38;2;{r};{g};{b}m")
 
-def setBackgroundColor(r,g,b):
+def setTTYBgCol(r,g,b): #Sets the color of the text background (if supported by the terminal)
     return(f"\x1b[48;2;{r};{g};{b}m")
