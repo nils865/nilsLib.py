6fcce8b91a17ea67becb910a419387bcdd957353
kruemmelbande <69861550+kruemmelbande@users.noreply.github.com>
2022-05-13 13:24:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='nilslib',
       version='0.1',
       description='A small collection of tools',
       url='https://github.com/nils865/nilsLib.py',
       author='Nils Staas',
       author_email='nils.staas0806@gmail.com',
       license='MIT',
-      packages=['nilstools'],
+      packages=['nilslib'],
       zip_safe=False)
