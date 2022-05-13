b40d90e8ebac32d42db05bcf3dd5bd6041811384
kruemmelbande <69861550+kruemmelbande@users.noreply.github.com>
2022-05-13 12:55:54 +0200

    Create setup.py

---
+++ nilslib/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nilslib',
+    version='1.0',
+    author='Nils',
+    description='Collection of small tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nils865/nilsLib.py',
+    license='MIT',
+    packages=['nilslib'],
+    install_requires=['requests'],
+)
