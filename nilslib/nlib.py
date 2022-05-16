860f2274dc285ba641d0cc8e4bbd9fd74be039c8
Nils <99215680+nils865@users.noreply.github.com>
2022-05-16 12:04:17 +0200

    changed formatting

--- nilslib/nlib.py
+++ nilslib/nlib.py
@@ -1,7 +1,7 @@
 import os, sys
     
 def clampDecimalPlace(comma, number): # Clamps the number to a specified number of decimal Places (e.g 0.123 >0.1)
-  return int(number * (10 ** comma)) / (10 ** comma)
+    return int(number * (10 ** comma)) / (10 ** comma)
 
 def setTTYFgCol(r,g,b): # Sets the color of the text foreground (if supported by the terminal)
     return(f"\x1b[38;2;{r};{g};{b}m")
